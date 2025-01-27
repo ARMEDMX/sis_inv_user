2c10889f5fa0ca7a6d50c6ee8d8d885c500ff95c
Conde <frontdevconde@gmail.com>
2025-01-26 18:34:07 -0600

    Cambios realizados para solucion de errores 6

--- config/app.php
+++ config/app.php
@@ -57,7 +57,7 @@ return [
 
     'url' => env('APP_URL', default: 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL'),
+    'asset_url' => env('ASSET_URL', null),
 
     /*
     |--------------------------------------------------------------------------
